366109afef3208114bd7d4f660695f84a6ae2e1d
ImYersain <ersainaldabaev@gmail.com>
2023-04-26 01:32:40 +0200

    f/upload files(audio, image) logic, FileService, static distribution

--- server/src/main.ts
+++ server/src/main.ts
@@ -5,11 +5,16 @@ const start = async () => {
     try {
         const PORT = 5000;
         const app = await NestFactory.create(AppModule);
-
+        app.enableCors();
         await app.listen(PORT, () => console.log(`server was started on port ${PORT}!`));
     } catch (error) {
         console.log(error);        
     }
 }
 
+// в сервисах(SERVICES) описываем логику
+// контроллерах(CONTROLLERS) работаем с запросами и ответами/ клиент-серверная составляющая
+// в схемах(SCHEMAS) описываются данные с которыми идет взаимодействие
+// в DTO описываются обьекты , которые ожидаются на вход в какие то функции в котроллерах   
+// все это подключается в app.module.ts(основной при запуске, указывается в main.ts)
 start();
--- server/src/track/track.controller.ts
+++ server/src/track/track.controller.ts
@@ -1,10 +1,11 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, UseInterceptors } from '@nestjs/common';
 import { Delete, Post } from '@nestjs/common/decorators/http/request-mapping.decorator';
 import { TrackService } from './track.service';
-import { Body, Param } from '@nestjs/common/decorators/http/route-params.decorator';
+import { Body, Param, Query, UploadedFiles } from '@nestjs/common/decorators/http/route-params.decorator';
 import { CreateTrackDto } from './dto/create-track.dto';
 import { ObjectId } from 'mongoose';
 import { CreateCommentDto } from './dto/create-comment.dto';
+import {FileFieldsInterceptor} from "@nestjs/platform-express";
 
 @Controller('/tracks') // @декораторы
 
@@ -12,13 +13,23 @@ export class TrackController {
     constructor(private trackService: TrackService) {} //за инжекченный сервис в контроллер
 
     @Post()
-    create(@Body() dto: CreateTrackDto) {
-        this.trackService.create(dto)
+    @UseInterceptors(FileFieldsInterceptor([
+        { name: 'picture', maxCount: 1 },
+        { name: 'audio', maxCount: 1 },
+    ]))
+    create(@UploadedFiles() files, @Body() dto: CreateTrackDto) {
+        const {picture, audio} = files
+        return this.trackService.create(dto, picture[0], audio[0]);
     }
 
     @Get()
-    getAll() {
-        return this.trackService.getAll();
+    getAll(@Query('count') count: number, @Query('offset') offset: number) { //count - количество треков по запросу, offset - c какого промежутка получать треки по новому запросу count
+        return this.trackService.getAll(count, offset);
+    }
+
+    @Get('/search')
+    search(@Query('query') query: string) {
+        return this.trackService.search(query);
     }
 
     @Get(':id')
@@ -34,5 +45,10 @@ export class TrackController {
     @Post('/comment')
     addComment(@Body() dto: CreateCommentDto) {
         return this.trackService.addComment(dto);
+    }
+
+    @Post('/listen/:id')
+    listen(@Param('id') id: ObjectId) {
+        return this.trackService.listen(id);
     }
 }
