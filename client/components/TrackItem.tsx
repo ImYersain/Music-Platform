962360de166004094f2bd574a35043ede28ba9fd
ImYersain <ersainaldabaev@gmail.com>
2023-04-30 01:55:51 +0200

    f/added styles modules for TrackItem with scss

--- client/components/TrackItem.tsx
+++ client/components/TrackItem.tsx
@@ -1,15 +1,29 @@
 import React, { FC } from 'react'
 import { ITrack } from '../types/tracks';
+import { Card, IconButton, Grid } from '@material-ui/core';
+import styles from '../styles/TrackItem.module.scss';
+import { PlayArrow, Pause, Delete } from '@material-ui/icons';
 
 interface ITrackItemProps {
     track: ITrack;
     active?: boolean;
 }
 
-export const TrackItem: FC<ITrackItemProps> = ({track, active}) => {
+export const TrackItem: FC<ITrackItemProps> = ({track, active = true}) => {
     return (
-        <div>
-            {track.name}
-        </div>
+        <Card className={styles.track}>
+            <IconButton>
+                {active? <Pause /> : <PlayArrow />}
+            </IconButton>
+            <img width={'70px'} height={'70'} src={track.picture} />
+            <Grid container direction={'column'} className={styles.description}>
+                <div>{track.name}</div>
+                <div className={styles.artist}>{track.artist}</div>
+            </Grid>
+            {active && <div>02:11/05:00</div>}
+            <IconButton className={styles.deleteTrack}>
+                <Delete />
+            </IconButton>
+        </Card>
     )
 }
--- client/pages/tracks/index.tsx
+++ client/pages/tracks/index.tsx
@@ -8,11 +8,11 @@ import { TrackList } from '../../components/TrackList';
 const Index = () => {
     const router = useRouter();
     const tracks: ITrack[] = [
-        {_id: '1', artist: 'Zayn', text: 'I dont wanna live forever', audio: 'https://localhost:5000/5133a554-3789-473c-a04e-8f929dd83221.mp3', picture: 'https://localhost:5000/28ebfcd5-0da4-4f2d-ae23-d4e942f6b398.jpg', comments: [{_id: '1', username: 'John', text: 'So so beatiful song!'}], listens: 3, name: 'Baby'},
+        {_id: '1', artist: 'Zayn', text: 'I dont wanna live forever', audio: 'http://localhost:5000/audio/900818bc-b11f-492b-ab81-15627fa0b418.mp3', picture: 'http://localhost:5000/image/b569afe2-d227-4fec-9fda-7286795b35df.jpg', comments: [{_id: '1', username: 'John', text: 'So so beatiful song!'}], listens: 3, name: 'Baby'},
 
-        {_id: '2', artist: 'BTR', text: 'We are', audio: 'https://localhost:5000/5133a554-3789-473c-a04e-8f929dd83221.mp3', picture: 'https://localhost:5000/28ebfcd5-0da4-4f2d-ae23-d4e942f6b398.jpg', comments: [{_id: '1', username: 'John', text: 'So so beatiful song!'}], listens: 3, name: 'Baby'},
+        {_id: '2', artist: 'BTR', text: 'We are', audio: 'http://localhost:5000/audio/900818bc-b11f-492b-ab81-15627fa0b418.mp3', picture: 'http://localhost:5000/image/b569afe2-d227-4fec-9fda-7286795b35df.jpg', comments: [{_id: '1', username: 'John', text: 'So so beatiful song!'}], listens: 3, name: 'We are'},
 
-        {_id: '3', artist: 'Scriptonite', text: 'VCVBBB', audio: 'https://localhost:5000/5133a554-3789-473c-a04e-8f929dd83221.mp3', picture: 'https://localhost:5000/28ebfcd5-0da4-4f2d-ae23-d4e942f6b398.jpg', comments: [{_id: '1', username: 'John', text: 'So so beatiful song!'}], listens: 3, name: 'Baby'}
+        {_id: '3', artist: 'Scriptonite', text: 'VCVBBB', audio: 'http://localhost:5000/audio/900818bc-b11f-492b-ab81-15627fa0b418.mp3', picture: 'http://localhost:5000/image/b569afe2-d227-4fec-9fda-7286795b35df.jpg', comments: [{_id: '1', username: 'John', text: 'So so beatiful song!'}], listens: 3, name: 'VCVBBB'}
     ];
 
     return (
