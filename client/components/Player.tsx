e6412415c4086017b9795fcc70a7278e8c6324bf
ImYersain <ersainaldabaev@gmail.com>
2023-05-04 17:53:01 +0200

    f/added on player component logic (play/pause, volume up/down)

--- client/components/Player.tsx
+++ client/components/Player.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {useEffect} from 'react'
 import { IconButton } from '@material-ui/core';
 import { Pause, PlayArrow } from '@material-ui/icons';
 import styles from '../styles/Player.module.scss';
@@ -6,15 +6,44 @@ import { ITrack } from '../types/tracks';
 import { Grid } from '@mui/material';
 import TrackProgress from './TrackProgress';
 import {VolumeUp} from '@material-ui/icons'
+import { useActions } from '../hooks/useActions';
+import { useSelector } from 'react-redux';
+import { useTypedSelector } from '../hooks/useTypedSelector';
+
+let audio: HTMLAudioElement;
 
 const Player = () => {
-    const track: ITrack =  {_id: '1', artist: 'Zayn', text: 'I dont wanna live forever', audio: 'http://localhost:5000/audio/900818bc-b11f-492b-ab81-15627fa0b418.mp3', picture: 'http://localhost:5000/image/b569afe2-d227-4fec-9fda-7286795b35df.jpg', comments: [{_id: '1', username: 'John', text: 'So so beatiful song!'}], listens: 3, name: 'Baby'};
-    const active = false;
+    const track: ITrack =  {_id: '1', artist: 'Zayn', text: 'I dont wanna live forever', audio: 'http://localhost:5000/audio/19c4d32e-105f-4819-be0f-b3d54e55dc55.mp3', picture: 'http://localhost:5000/image/b569afe2-d227-4fec-9fda-7286795b35df.jpg', comments: [{_id: '1', username: 'John', text: 'So so beatiful song!'}], listens: 3, name: 'Baby'};
+    const {active, currentTime, duration, volume, pause} = useTypedSelector((state) => state.playerReducer);
+    const {playTrack, pauseTrack, setVolume} = useActions();
+
+    useEffect(() => {
+        if(!audio) {
+            audio = new Audio();
+            audio.src = track.audio;
+            audio.volume = 1;
+        }
+    }, [])
+
+    const onPlay = () => {
+        if(pause) {
+            playTrack();
+            audio.play();
+        } else {
+            pauseTrack();
+            audio.pause();
+        }
+    }
+
+    const onChangeVolume = (e: React.ChangeEvent<HTMLInputElement>) => {
+        audio.volume = +e.target.value / 100;
+        setVolume(+e.target.value);
+    }
 
     return (
         <div className={styles.player}>
-            <IconButton onClick={(e) => e.stopPropagation()}>
-                {active? <Pause /> : <PlayArrow />}
+            <IconButton onClick={onPlay}>
+                {pause? <PlayArrow /> : <Pause />}
             </IconButton>
             <Grid container direction={'column'} className={styles.description}>
                 <div>{track.name}</div>
@@ -22,7 +51,7 @@ const Player = () => {
             </Grid>
             <TrackProgress left={0} right={100} onChange={() => {}} />
             <VolumeUp style={{marginLeft: 'auto'}} />
-            <TrackProgress left={0} right={100} onChange={() => {}} />
+            <TrackProgress left={volume} right={100} onChange={onChangeVolume} />
         </div>
     )
 }
